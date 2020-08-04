bad478f7120a222c8579ed4cb4028731179a4fd4
Yannis Sgarra <hello@yannissgarra.com>
2020-08-04 23:09:46 +0200

    chore(api-gateway): install profiler

--- apps/api-gateway/app/config/bundles.php
+++ apps/api-gateway/app/config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['local' => true, 'test' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['local' => true, 'test' => true],
 ];

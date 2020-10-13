2722fcc74287cb87fc4e1729090a189559196538
Romain Lisowski <romain.lisowski@gmail.com>
2020-10-13 17:05:20 +0200

    feat(front-web): Page title

--- apps/front-web/app/vue.config.js
+++ apps/front-web/app/vue.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   devServer: { https: true },
-
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      // eslint-disable-next-line no-param-reassign
+      args[0].title = 'PinkStory'
+      return args
+    })
+  },
   pluginOptions: {
     i18n: {
       locale: 'fr',

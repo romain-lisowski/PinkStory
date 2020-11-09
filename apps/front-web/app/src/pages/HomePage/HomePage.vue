77a38e9347dd0838ec7910a5c00b059672d21abd
Romain Lisowski <romain.lisowski@gmail.com>
2020-11-09 20:55:14 +0100

    feat(front-web): Improve category components

--- apps/front-web/app/src/pages/HomePage/HomePage.vue
+++ apps/front-web/app/src/pages/HomePage/HomePage.vue
@@ -3,7 +3,23 @@
     <HomeMainStory />
     <StoryTopRated />
     <StoryLatest />
-    <CategoryBlock :display-search-link="true" />
+    <CategoryBlock>
+      <template v-slot:header>
+        <div
+          class="flex flex-col sm:flex-row justify-between mb-8 pt-8 sm:pt-12"
+        >
+          <p class="text-3xl sm:text-4xl xl:text-5xl font-semibold text-left">
+            {{ $t('categories') }}
+          </p>
+          <router-link
+            :to="{ name: 'Search' }"
+            class="pt-2 sm:pt-4 xl:pt-6 text-lg sm:text-xl xl:text-2xl text-left sm:text-right text-accent cursor-pointer"
+          >
+            {{ $t('search-story') }} >
+          </router-link>
+        </div>
+      </template>
+    </CategoryBlock>
   </div>
 </template>
 
@@ -23,3 +39,12 @@ export default {
   },
 }
 </script>
+
+<i18n>
+{
+  "fr": {
+    "categories": "Catégories",
+    "search-story": "Rechercher des histoires"
+  }
+}
+</i18n>
--- apps/front-web/app/src/pages/SearchPage/SearchPage.vue
+++ apps/front-web/app/src/pages/SearchPage/SearchPage.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="mb-10">
-    <CategoryBlock />
+    <CategoryBlock>
+      <template v-slot:header>
+        <div
+          class="flex flex-col sm:flex-row justify-between mb-8 pt-8 sm:pt-12"
+        >
+          <p
+            class="block w-full mt-12 sm:mt-0 text-4xl md:text-5xl xl:text-6xl font-semibold text-center"
+          >
+            {{ $t('search') }}
+          </p>
+        </div>
+      </template>
+      <template v-slot:footer>
+        <p
+          class="block pt-2 sm:pt-4 xl:pt-6 w-full text-lg sm:text-xl xl:text-2xl text-center text-accent"
+        >
+          {{ results + ' ' + $t('results') }}
+        </p>
+      </template>
+    </CategoryBlock>
     <div class="mx-8">
       <StoryList />
     </div>
@@ -17,13 +36,19 @@ export default {
     CategoryBlock,
     StoryList,
   },
+  data() {
+    return {
+      results: 32,
+    }
+  },
 }
 </script>
 
 <i18n>
 {
   "fr": {
-    "search": "Rechercher"
+    "search": "Rechercher par catégorie",
+    "results": "histoires correspondantes"
   }
 }
 </i18n>

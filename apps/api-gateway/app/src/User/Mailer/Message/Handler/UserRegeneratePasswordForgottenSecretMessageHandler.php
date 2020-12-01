b8e276886784a59dce8ec29281a4ec6ddfa763b2
Yannis Sgarra <hello@yannissgarra.com>
2020-12-01 18:11:32 +0100

    fix(api-gateway): sync message handlers needed

---
+++ apps/api-gateway/app/src/User/Mailer/Message/Handler/UserRegeneratePasswordForgottenSecretMessageHandler.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\User\Mailer\Message\Handler;
+
+use App\User\Message\UserRegeneratePasswordForgottenSecretMessage;
+use App\User\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Messenger\Handler\MessageSubscriberInterface;
+
+final class UserRegeneratePasswordForgottenSecretMessageHandler implements MessageSubscriberInterface
+{
+    private EntityManagerInterface $entityManager;
+    private UserRepository $repository;
+
+    public function __construct(EntityManagerInterface $entityManager, UserRepository $repository)
+    {
+        $this->entityManager = $entityManager;
+        $this->repository = $repository;
+    }
+
+    public function __invoke(UserRegeneratePasswordForgottenSecretMessage $message)
+    {
+        $user = $this->repository->findOne($message->getId());
+
+        // TODO: send mail
+    }
+
+    public static function getHandledMessages(): iterable
+    {
+        yield UserRegeneratePasswordForgottenSecretMessage::class => [
+            'from_transport' => 'sync',
+        ];
+    }
+}

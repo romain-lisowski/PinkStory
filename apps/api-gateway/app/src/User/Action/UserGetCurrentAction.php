e88d8b0b237520dd739822c0752b975e344c124c
Yannis Sgarra <hello@yannissgarra.com>
2020-12-02 10:47:43 +0100

    refactor(api-gateway): get current user is for update

--- apps/api-gateway/app/src/User/Action/UserGetCurrentAction.php
+++ apps/api-gateway/app/src/User/Action/UserGetCurrentAction.php
@@ -6,24 +6,29 @@ namespace App\User\Action;
 
 use App\Responder\ResponderInterface;
 use App\User\Security\UserSecurityInterface;
+use App\User\Voter\UserableVoter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Throwable;
 
 /**
  * @IsGranted("ROLE_USER")
- * @Route("/users/current", name="user_current", methods={"GET"})
+ * @Route("/users/current/update", name="user_current_update", methods={"GET"})
  */
 final class UserGetCurrentAction
 {
+    private AuthorizationCheckerInterface $authorizationChecker;
     private ResponderInterface $responder;
     private UserSecurityInterface $security;
 
-    public function __construct(ResponderInterface $responder, UserSecurityInterface $security)
+    public function __construct(AuthorizationCheckerInterface $authorizationChecker, ResponderInterface $responder, UserSecurityInterface $security)
     {
+        $this->authorizationChecker = $authorizationChecker;
         $this->responder = $responder;
         $this->security = $security;
     }
@@ -31,6 +36,10 @@ final class UserGetCurrentAction
     public function __invoke(Request $request): Response
     {
         try {
+            if (false === $this->authorizationChecker->isGranted(UserableVoter::UPDATE, $this->security->getUser())) {
+                throw new AccessDeniedException();
+            }
+
             return $this->responder->render([
                 'user' => $this->security->getUser(),
             ], ['groups' => 'full']);

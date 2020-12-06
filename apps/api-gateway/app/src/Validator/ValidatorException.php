569d98410867f5f76b14e83aba30c090aa3b6538
Yannis Sgarra <hello@yannissgarra.com>
2020-12-06 13:15:02 +0100

    fix(api-gateway): wrong iterator item method

--- apps/api-gateway/app/src/Validator/ValidatorException.php
+++ apps/api-gateway/app/src/Validator/ValidatorException.php
@@ -26,7 +26,7 @@ final class ValidatorException extends Exception implements HasErrorsExceptionIn
     public function populateErrors(Traversable $errors): void
     {
         foreach ($errors as $error) {
-            $this->errors[$error->getOrigin()->getName()] = $error->getMessage();
+            $this->errors[$error->getPropertyPath()] = $error->getMessage();
         }
     }
 }

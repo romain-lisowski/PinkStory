1728457917491e9e0153bd7e9ff811a46bcc4445
Yannis Sgarra <hello@yannissgarra.com>
2020-12-16 22:50:59 +0100

    fix(api-gateway): user is editable from medium

--- apps/api-gateway/app/src/User/Model/Dto/UserFull.php
+++ apps/api-gateway/app/src/User/Model/Dto/UserFull.php
@@ -5,33 +5,20 @@ declare(strict_types=1);
 namespace App\User\Model\Dto;
 
 use App\Language\Model\Dto\Language;
-use App\User\Model\UserEditableInterface;
-use App\User\Model\UserEditableTrait;
 use DateTime;
 use Symfony\Component\Serializer\Annotation as Serializer;
 
-final class UserFull extends UserMedium implements UserEditableInterface
+final class UserFull extends UserMedium
 {
-    // Not use dto one, to avoid circular reference with serializer.
-    use UserEditableTrait;
-
     /**
      * @Serializer\Groups({"serializer"})
      */
     private string $email;
 
-    /**
-     * Cause we don't use dto one.
-     *
-     * @Serializer\Groups({"serializer"})
-     */
-    private bool $editable = false;
-
     public function __construct(string $id = '', bool $imageDefined = false, string $name = '', string $nameSlug = '', string $email = '', DateTime $createdAt, Language $language)
     {
         parent::__construct($id, $imageDefined, $name, $nameSlug, $createdAt, $language);
 
-        $this->user = $this;
         $this->email = $email;
     }
 
--- apps/api-gateway/app/src/User/Model/Dto/UserMedium.php
+++ apps/api-gateway/app/src/User/Model/Dto/UserMedium.php
@@ -7,11 +7,15 @@ namespace App\User\Model\Dto;
 use App\Language\Model\Dto\Language;
 use App\Language\Model\Dto\LanguageableTrait;
 use App\Language\Model\LanguageableInterface;
+use App\User\Model\UserEditableInterface;
+use App\User\Model\UserEditableTrait;
 use DateTime;
 use Symfony\Component\Serializer\Annotation as Serializer;
 
-class UserMedium extends User implements LanguageableInterface
+class UserMedium extends User implements UserEditableInterface, LanguageableInterface
 {
+    // Not use dto one, to avoid circular reference with serializer.
+    use UserEditableTrait;
     use LanguageableTrait;
 
     /**
@@ -29,10 +33,18 @@ class UserMedium extends User implements LanguageableInterface
      */
     private DateTime $createdAt;
 
+    /**
+     * Cause we don't use dto one.
+     *
+     * @Serializer\Groups({"serializer"})
+     */
+    private bool $editable = false;
+
     public function __construct(string $id = '', bool $imageDefined = false, string $name = '', string $nameSlug = '', DateTime $createdAt, Language $language)
     {
         parent::__construct($id, $imageDefined);
 
+        $this->user = $this;
         $this->name = $name;
         $this->nameSlug = $nameSlug;
         $this->createdAt = $createdAt;

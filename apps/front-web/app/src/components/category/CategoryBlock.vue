e4989cc70bd058206bf98cf070c45510994e4779
Romain Lisowski <romain.lisowski@gmail.com>
2020-12-24 18:47:57 +0100

    refactor(front-web): Remove Suspense from categoryBlock

--- apps/front-web/app/src/components/category/CategoryBlock.vue
+++ apps/front-web/app/src/components/category/CategoryBlock.vue
@@ -1,39 +1,26 @@
 <template>
-  <Suspense>
-    <template #default>
-      <div
-        id="category"
-        class="sm:mt-16 pb-4 sm:pb-12 bg-primary-inverse bg-opacity-5"
-      >
-        <div class="mx-12">
-          <slot name="header"></slot>
+  <div
+    id="category"
+    class="sm:mt-16 pb-4 sm:pb-12 bg-primary-inverse bg-opacity-5"
+  >
+    <div class="mx-12">
+      <slot name="header"></slot>
 
-          <CategoryList
-            v-for="(categoryList, index) in data.categoryLists"
-            :key="index"
-            :category-list="categoryList"
-          />
+      <CategoryList
+        v-for="(categoryList, index) in data.categoryLists"
+        :key="index"
+        :category-list="categoryList"
+      />
 
-          <slot name="footer"></slot>
-        </div>
-      </div>
-    </template>
-
-    <template #fallback>
-      <div class="sm:mt-16 pb-4 sm:pb-12 bg-primary-inverse bg-opacity-5">
-        <div class="mx-12">
-          {{ t('loading') }}
-        </div>
-      </div>
-    </template>
-  </Suspense>
+      <slot name="footer"></slot>
+    </div>
+  </div>
 </template>
 
 <script>
 import CategoryList from '@/components/category/CategoryList.vue'
 import ApiStoryThemes from '@/api/ApiStoryThemes'
 import { useStore } from 'vuex'
-import { useI18n } from 'vue-i18n'
 import { onMounted, reactive } from 'vue'
 
 export default {
@@ -57,16 +44,7 @@ export default {
       }
     })
 
-    const { t } = useI18n({
-      locale: 'fr',
-      messages: {
-        fr: {
-          loading: 'Chargement des catégories ...',
-        },
-      },
-    })
-
-    return { data, t }
+    return { data }
   },
 }
 </script>

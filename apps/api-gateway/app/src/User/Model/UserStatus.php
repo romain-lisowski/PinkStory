f47cf9d97e6aff594ce43f8540392a8fd1e28617
Yannis Sgarra <hello@yannissgarra.com>
2020-12-29 14:27:05 +0100

    refactor(api-gateway): enum

--- apps/api-gateway/app/src/User/Model/UserStatus.php
+++ apps/api-gateway/app/src/User/Model/UserStatus.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace App\User\Model;
 
-final class UserStatus
+use App\Model\AbstractEnum;
+
+final class UserStatus extends AbstractEnum
 {
     public const ACTIVATED = 'ACTIVATED';
     public const BLOCKED = 'BLOCKED';
+
+    protected static string $translationPrefix = 'user_status.value.';
 }

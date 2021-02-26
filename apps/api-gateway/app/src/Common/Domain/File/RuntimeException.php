648d7c3583bacc60d2c941a35ddec5ed80722281
Yannis Sgarra <hello@yannissgarra.com>
2021-02-26 19:17:38 +0100

    fix(api-gateway): missing strict types

--- apps/api-gateway/app/src/Common/Domain/File/RuntimeException.php
+++ apps/api-gateway/app/src/Common/Domain/File/RuntimeException.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Common\Domain\File;
 
 class RuntimeException extends \RuntimeException implements ExceptionInterface
--- apps/api-gateway/app/src/Common/Infrastructure/Request/ParamConverter/RuntimeException.php
+++ apps/api-gateway/app/src/Common/Infrastructure/Request/ParamConverter/RuntimeException.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Common\Infrastructure\Request\ParamConverter;
 
 class RuntimeException extends \RuntimeException implements ExceptionInterface

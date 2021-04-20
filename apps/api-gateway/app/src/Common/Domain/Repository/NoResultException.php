f425a2fa5f12d397145b43b377b6a26049c7ad1d
Yannis Sgarra <hello@yannissgarra.com>
2021-04-20 13:10:18 +0200

    fix(api-gateway): sometimes there is no previous error

--- apps/api-gateway/app/src/Common/Domain/Repository/NoResultException.php
+++ apps/api-gateway/app/src/Common/Domain/Repository/NoResultException.php
@@ -8,8 +8,12 @@ class NoResultException extends RuntimeException
 {
     protected $message = 'repository.exception.no_result_exception';
 
-    public function __construct(\Throwable $e)
+    public function __construct(?\Throwable $e = null)
     {
-        parent::__construct($this->message, $e->getCode(), $e);
+        if (null !== $e) {
+            parent::__construct($this->message, $e->getCode(), $e);
+        } else {
+            parent::__construct($this->message);
+        }
     }
 }

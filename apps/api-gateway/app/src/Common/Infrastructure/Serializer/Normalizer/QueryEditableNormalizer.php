ce8018538eb6b393737482e7b6cf3e9ae631eb5b
Yannis Sgarra <hello@yannissgarra.com>
2021-04-22 12:42:01 +0200

    feat(api-gateway): use query editable interface for serializing

---
+++ apps/api-gateway/app/src/Common/Infrastructure/Serializer/Normalizer/QueryEditableNormalizer.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Common\Infrastructure\Serializer\Normalizer;
+
+use App\Common\Query\Model\EditableInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
+
+final class QueryEditableNormalizer implements ContextAwareNormalizerInterface, NormalizerAwareInterface
+{
+    use NormalizerAwareTrait;
+
+    private AuthorizationCheckerInterface $authorizationChecker;
+
+    public function __construct(AuthorizationCheckerInterface $authorizationChecker)
+    {
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize($editable, string $format = null, array $context = [])
+    {
+        // avoid circular reference
+        if (false === isset($context[static::class.'.used'])) {
+            $context[static::class.'.used'] = [];
+        }
+        $context[static::class.'.used'][] = $editable;
+
+        $editable->setEditable($this->authorizationChecker->isGranted(EditableInterface::UPDATE, $editable));
+
+        return $this->normalizer->normalize($editable, $format, $context);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsNormalization($data, string $format = null, array $context = []): bool
+    {
+        // avoid circular reference
+        if (
+            false === empty($context[static::class.'.used'])
+            && true === is_array($context[static::class.'.used'])
+            && in_array($data, $context[static::class.'.used'])
+        ) {
+            return false;
+        }
+
+        return $data instanceof EditableInterface;
+    }
+}
--- apps/api-gateway/app/src/Common/Query/Model/AbstractModel.php
+++ apps/api-gateway/app/src/Common/Query/Model/AbstractModel.php
@@ -4,8 +4,19 @@ declare(strict_types=1);
 
 namespace App\Common\Query\Model;
 
-use App\Common\Domain\Model\EditableInterface;
-
 abstract class AbstractModel implements EditableInterface
 {
+    private bool $editable = false;
+
+    public function getEditable(): bool
+    {
+        return $this->editable;
+    }
+
+    public function setEditable(bool $editable): self
+    {
+        $this->editable = $editable;
+
+        return $this;
+    }
 }

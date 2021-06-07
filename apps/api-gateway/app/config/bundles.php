403a2cd48b051131bb4907d06e7d651f67c7431a
Yannis Sgarra <hello@yannissgarra.com>
2021-06-07 17:14:09 +0200

    feat(api-gateway): allow cors

--- apps/api-gateway/app/config/bundles.php
+++ apps/api-gateway/app/config/bundles.php
@@ -15,4 +15,5 @@ return [
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];

458191ea7040ef8f17dba0881113f45f7949cb1c
Romain Lisowski <romain.lisowski@gmail.com>
2021-06-09 14:15:48 +0200

    refactor(front-web): Simple useFetch

--- apps/front-web/app/src/composition/api/useFetch.js
+++ apps/front-web/app/src/composition/api/useFetch.js
@@ -5,44 +5,16 @@ const baseUrl = process.env.VUE_APP_API_URL
 export default (method, uri, body = null, jwt = null) => {
   const state = reactive({ response: null, error: null, isLoading: false })
 
-  const defaultSearchParams = (method) => {
-    const searchParams = new URLSearchParams()
-
-    if (!['GET', 'POST', 'PATCH', 'DELETE'].includes(method)) {
-      throw new Error('Method invalid', method)
-    }
-
-    if (method === 'POST' || method === 'PATCH') {
-      searchParams.append('_method', 'POST')
-    } else {
-      searchParams.append('_method', 'GET')
-    }
-
-    searchParams.append('_locale', 'fr')
-
-    return searchParams.toString()
-  }
-
-  // Create URL and add default search params
-  const urlWithDefaultSearchParams = (method, uri) => {
-    if (typeof uri !== 'string') {
-      throw new Error('URI must be a string', uri)
-    }
-
-    const url = new URL(`${baseUrl}/${uri}`)
-    url.search = defaultSearchParams(method)
-    return url
-  }
-
   const fetchData = async () => {
     state.isLoading = true
 
-    const url = urlWithDefaultSearchParams(method, uri)
+    const url = new URL(`${baseUrl}/${uri}?_locale=fr`)
     const options = {
       method,
       headers: { Authorization: jwt ? `Bearer ${jwt}` : null },
     }
 
+    // GET request can't have a body
     if (body !== null) {
       options.body = JSON.stringify(body)
     }

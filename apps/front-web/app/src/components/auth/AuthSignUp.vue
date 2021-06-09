00576edeec29d7ea0622f799585a970453023fbc
Romain Lisowski <romain.lisowski@gmail.com>
2021-06-09 16:49:13 +0200

    style(front-web): remove console.log

--- apps/front-web/app/src/components/auth/AuthSignUp.vue
+++ apps/front-web/app/src/components/auth/AuthSignUp.vue
@@ -91,7 +91,6 @@ export default {
     watch(
       () => [data.password, data.passwordConfirm],
       () => {
-        console.log('validate')
         validatePasswordsMatching()
       }
     )
